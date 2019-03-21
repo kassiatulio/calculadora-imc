1702657ad968ea593bb411afcfb84001358c238d
kassiatulio <kassia.tulio@gmail.com>
2019-03-21 23:29:05 +0000

    Implement IMC calculator

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+const submitHandler = (event) => {
+  event.preventDefault()
+  
+  const person = buildPersonFromSubmitEvent(event)
+  const imc = calculateImc(person.peso, person.altura)
+
+  showMessage(`Seu IMC é: ${imc}.`)
+}
+
+const buildPersonFromSubmitEvent = (event) => {
+  const person = {
+    peso: event.target[0].value,
+    altura: event.target[1].value,
+    sexo: event.target[2].value,
+  }
+
+  return person
+}
+
+const calculateImc = (peso, altura) => {
+  return Math.floor(peso / Math.pow((altura / 100), 2))
+}
+
+const showMessage = (message) => {
+  alert(message)
+}
+
+window.onload = () => {
+  const form = document.querySelector('.form')
+  
+  form.onsubmit = submitHandler
+}
